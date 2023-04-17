48892c87817fd985e89f58f69a36bf0af2f003a8
nikita23092000 <122999774+nikita23092000@users.noreply.github.com>
2023-04-17 21:24:59 +0300

    Добавить цвет

---
+++ TopMessenger/Infrastruckture/Utils/ValidatuionUtills.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopMessenger.Infrastruckture.Utils
+{
+    public class ValidatuionUtills
+    {
+        public static bool EmptyStrValidate(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return false;
+            return true;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+
+        public static bool DigitContainsValidate(string value)
+        {
+            foreach (var item in value)
+            {
+                if (char.IsDigit(item)==true)
+                {
+                    return false;
+                }
+                
+            }
+            return true;
+
+        }
+        /// <summary>
+        /// Значение является валидными сли есть спец. символы
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        
+        public static bool SymbContainsValidate(string value)
+        {
+            foreach (var item in value)
+            {
+                if (item == '^')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool EmailValidate(string value)
+        {
+            foreach (var item in value)
+            {
+                if (item == '@')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
